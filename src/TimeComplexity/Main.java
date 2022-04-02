e57e5f71862754be7321f5209bc469b8802ddf59
seonghun.han <hsh931212a@naver.com>
2022-04-02 15:08:39 +0900

    codingTest base on JAVA

--- src/TimeComplexity/Main.java
+++ src/TimeComplexity/Main.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         test test = new test();
-        Scanner scan = new Scanner(System.in);
-        int size = scan.nextInt();
-        System.out.println(size);
-        test.func1(size);
+        //Scanner scan = new Scanner(System.in);
+        //int[] size = scan.nextInt();
+//        System.out.println(size);
+//        test.func1(size);
 
-        System.out.println(test.func1(size));
+        //int[] arr1 = {1,52,48};
+        //test.fun2(arr1, 3);
+        //System.out.println( test.fun2(arr1, 3));
+
+        System.out.println(test.fun4(10));
+
     }
 }
--- src/TimeComplexity/test.java
+++ src/TimeComplexity/test.java
@@ -19,4 +19,33 @@ public class test {
         }
         return sum;
     };
+
+    public int fun2(int [] arr, int N){
+        for(int i = 0; i < N; i++){
+            for(int j = i + 1; j < N; i++){
+                if(i+j <= 100){
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int fun3(int N) {
+        for(int i = 0; i < N+1; i++){
+            if(i*i == N){
+                return 1;
+            }
+        }return 0;
+    }
+
+    public int fun4(int N){
+        int val1 = 1;
+        while(2*val1 <= N)
+            val1 *= 2;
+            System.out.println("asd");
+
+        return val1;
+    }
+
 }
